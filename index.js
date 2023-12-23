bf19982578194e10dfc972c1a55142e7b9a3f897
Victor Iglunov <116027529+WebArchitect89@users.noreply.github.com>
2023-12-23 15:26:16 +0000

    created the ligic for the randomasation/linked to the css

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
+
+app.use(express.static("public"));
+
 //Step 3 - Make the styling show up.
 //Hint 1: CSS files are static files!
 //Hint 2: The header and footer are partials.
@@ -15,10 +18,22 @@ const port = 3000;
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
+
+  res.render("index.ejs")
   //Step 1 - Make the get route work and render the index.ejs file.
 });
 
 app.post("/submit", (req, res) => {
+
+var randomNumber = Math.floor(Math.random()*adj.length);
+var randomName = adj[randomNumber] + " " + noun[randomNumber]
+
+res.render("index.ejs",{
+
+  theName: randomName
+
+});
+
   //Step 2 - Make the generate name functionality work
   //Hint: When the "Generate Name" button in index.ejs is clicked, it should hit up this route.
   //Then:
